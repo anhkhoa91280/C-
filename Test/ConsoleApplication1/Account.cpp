82dde8690279f6a599ac1e70803d91367673d2bc
Dang Nguyen Anh Khoa <anhkhoa91280@gmail.com>
2021-05-25 13:11:28 +0700

    update files

--- Test/ConsoleApplication1/Account.cpp
+++ Test/ConsoleApplication1/Account.cpp
@@ -1,7 +1,7 @@
 #include "Account.h";
 
 Account::Account(void) {
-	account_num = "#dummy account12345";
+	account_num = "#dummy account123456";
 }
 std::string Account::getAccNum() {
 	return account_num;
